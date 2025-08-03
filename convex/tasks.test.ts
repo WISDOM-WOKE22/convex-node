47e7405c23d02a04cdaff91448b7ddacafa6ed73
WISDOM-WOKE22 <wisdomwokedev@gmail.com>
2025-08-03 09:54:26 +0100

    chore: updated test script

--- convex/tasks.test.ts
+++ convex/tasks.test.ts
@@ -5,12 +5,30 @@ import schema from "./schema";
 
 test("Creating tasks", async () => {
   const t = convexTest(schema);
-  const id = await t.mutation(api.tasks.create, { title: "Buy groceries", isCompleted: false } );
-  await t.mutation(api.tasks.update, { id, title: "Buy groceries", isCompleted: true } );
-  await t.mutation(api.tasks.deleteTask, { id } );
-  const tasks = await t.query(api.tasks.get);
-  console.log(tasks);
-  // expect(tasks).toMatchObject([
-  //   { title: "Buy groceries", isCompleted: false }
-  // ]);
+
+  // Create a task
+  const id = await t.mutation(api.tasks.create, {
+    title: "Buy groceries",
+    isCompleted: false,
+  });
+
+  // Update the task
+  await t.mutation(api.tasks.update, {
+    id,
+    title: "Buy groceries",
+    isCompleted: true,
+  });
+
+  // Get tasks (should contain the updated task)
+  let tasks = await t.query(api.tasks.get, {});
+  expect(tasks).toMatchObject([
+    { title: "Buy groceries", isCompleted: true },
+  ]);
+
+  // Delete the task
+  await t.mutation(api.tasks.deleteTask, { id });
+
+  // Check tasks again (should be empty now)
+  tasks = await t.query(api.tasks.get, {});
+  expect(tasks).toEqual([]);
 });
